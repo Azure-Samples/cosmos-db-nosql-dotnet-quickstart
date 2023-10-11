6a45db9605ce09d6188dec7c4411e5484ae886d1
Sidney Andrews <sidandrews@microsoft.com>
2023-10-10 21:43:35 -0400

    Update comment section names

--- src/web/Program.cs
+++ src/web/Program.cs
@@ -6,12 +6,15 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
 builder.Services.AddSingleton<CosmosClient>((_) =>
+{
     // <create_client>
-    new CosmosClient(
+    CosmosClient client = new(
         accountEndpoint: builder.Configuration["AZURE_COSMOS_DB_NOSQL_ENDPOINT"]!,
         tokenCredential: new DefaultAzureCredential()
+    );
     // </create_client>
-));
+    return client;
+});
 
 var app = builder.Build();
 
